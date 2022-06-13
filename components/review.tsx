55605aea21d51316a6e5da9a5e7c774bdccc25f5
Loai AlFarran <loayei@gmail.com>
2022-06-13 15:23:38 -0700

    Logo fixed

--- components/review.tsx
+++ components/review.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const Review: React.FC<Props> = ({ children, by }) => (
     <div className="flex flex-col justify-center text-center px-5 md:px-10 lg:px-20">
-        <div className="text-xl  md:text-2xl leading-10 lg:leading-[3rem] tracking-tight">
+        <div className="text-xl md:text-2xl leading-10 lg:leading-[3rem] tracking-tight">
             &ldquo;{children}&rdquo;
         </div>
         <div className="mt-6">&mdash; {by}</div>
