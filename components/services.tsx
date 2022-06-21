52a8adcbd29394c6bcb65a1ad7b927ffad4a37ca
Loai AlFarran <loayei@gmail.com>
2022-06-21 12:59:47 -0700

    Car wrap section

--- components/services.tsx
+++ components/services.tsx
@@ -19,19 +19,31 @@ const Services = () => (
         renderContent={({ progress }) => (
           <ServiceContainer>
             <ServiceLeft progress={progress}>
-              <div>We Give Your Vehicle a </div>
-              <div className="text-4xl md:text-5xl font-semibold tracking-tight">
-                New LOOK
+              <div className="text-center text-lg md:text-5xl font-bold">Car Wraps </div>
+              <div className="py-2 px-10 lg:px-10 text-xs text-center md:text-sm font-semibold tracking-tight">
+                3M, Avery Dennison, and more. Car Wraps provide your
+                vehicle a one-of-a-kind appearance with the highest level of craftsmanship. Do you
+                want to give your car a fresh style while while conserving the OEM paint? Vinyl wraps from B7 Customs
+                will protect the paint underneath and can be removed afterwards, leaving the paint in its original state.
               </div>
             </ServiceLeft>
             <ServiceRight progress={progress}>
               <Image
-                src="/assets/tint.jpeg"
+                src="/assets/test.jpg"
                 layout="responsive"
-                width={1620}
-                height={750}
+                width={500}
+                height={400}
                 alt="Loayei"
+                className="rounded-xl"
               />
+              <div className=" py-2 text-black text-center text-sm font-bold md:text-sm tracking-tight">
+                We have over 500 different finishes to pick from so you can get the precise look you want.
+                Gloss, Matte, and Chrome vinyl are just a few of the options.
+                Complete or partial wraps, and full graphics are all available. Your vehicle can be given a variety of looks with a vinyl wrap.
+                A three-year warranty against bubbling and peeling is included. *** Your personalized wrap should last between 4 and 7 years.
+
+              </div>
+
             </ServiceRight>
           </ServiceContainer>
         )}
